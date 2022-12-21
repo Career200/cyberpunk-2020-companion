662d221ffd3280e710d69d4d12f1ccb3a7eb461b
Career404 <105638831+Career404@users.noreply.github.com>
2022-12-21 16:50:50 +0400

    feat: initial work on lifepath generator

---
+++ lifepath/lifepath-generator.js
@@ -0,0 +1,8 @@
+function startGenerator() {
+  console.log('Start generation');
+  const startButton = document.getElementById('start');
+  const characterWindow = document.getElementById('character');
+  startButton.style.bottom = '20px'; startButton.style.height = '40px'; startButton.style.width = '90px'; startButton.style.lineHeight = '20px'; startButton.style.fontSize = '20px'; start.innerHTML = 'New Lifepath'
+  characterWindow.style.height='60%'
+
+}
